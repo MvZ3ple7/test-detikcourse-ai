3ad7a1f0cc15538ab35286646ad68a7071ddc06a
MvZ <38483422+MvZ3ple7@users.noreply.github.com>
2024-05-22 20:16:12 +0700

    Update meal_planner.py

--- meal_planner.py
+++ meal_planner.py
@@ -14,7 +14,7 @@ def ask_gpt3_turbo(message, chat_log=None):
         messages=[
             {
                 "role": "system",
-                "content": "Anda adalah AI agent yang bertindak sebagai chef andal yang bisa memikirikan resep makan dari bahan-bahan yang terbatas. Anda adalah chef yang sangat ahli dalam masakan Indonesia dan nusantara.",
+                "content": "Anda adalah AI agent yang bertindak sebagai navigator handal yang bisa memberikan rute perjalanan untuk setiap kurir yang akan bertugas. Anda adalah navigator yang sangat ahli dalam perjalanan di Indonesia khususnya di daerah Jakarta dan Jawa Barat. Setiap kurir tidak akan mengunjungi tempat yang sama dalam satu kali perjalanan.",
             },
             {"role": "user", "content": message},
         ],
@@ -25,56 +25,92 @@ def ask_gpt3_turbo(message, chat_log=None):
 
 # Streamlit app
 def main():
-    st.title("Detikfood Meal Prep Planner")
+    st.title("Rute perjalanan driver")
 
-    bahan = st_tags(label="Daftarkan bahan-bahan di dapur Anda")
-    makanan = st_tags(
-        label="(optional) Sebutkan makanan apa saja yang sudah Anda masak"
+    # lokasi_keberangkatan = st.text_input(
+    #     "Berangkat darimana? (Misalnya: Sunter, Grogol, PIK)"
+    # )
+    list_lokasi_keberangkatan = st.radio(
+        "Lokasi keberangkatan?", ("Ruko ITC Cempaka Mas (Jakarta Pusat)")
     )
-    daerah = st.text_input(
-        "(optional) Anda mencari resep khas daerah mana? (Misalnya: Jawa, Sumatera, Bali)"
+    jumlah_driver = st.number_input(
+        "Untuk berapa orang Driver yang akan bertugas hari ini?", min_value=1, max_value=5, step=1
     )
-    ramah_anak = st.radio("Opsi makanan ramah anak", ("Tidak", "Ya"))
-    agama_budaya = st.text_input(
-        "(optional) Apakah Anda memiliki batasan seperti halal atau vegetarian?"
+    list_lokasi = st.multiselect("Preferensi lokasi",
+     [
+       "Mall Taman Anggrek (Jakarta Pusat)"
+     , "Mall Kelapa Gading (Jakarta Utara)"
+     , "Mall Sunter (Jakarta Utara)"
+     , "Mall Artha Gading (Jakarta Utara)"
+     , "Mall Ciputra (Jakarta Barat)"
+     , "Aeon Mall Bekasi (Bekasi)"
+     , "Aeon Mall Serpong (Tangerang)"
+     , "Botani Square Mall Bogor (Bogor)"
+     , "AEON Mall Sentul City (Bogor)"
+     , "Pondok Indah Mall (Jakarta Selatan)"
+     , "Mall Ciputra Tangerang (Tangerang)"
+     , "Summarecon Mall Serpong (Tangerang)"
+     , "Living World Alam Sutera (Tangerang)"
+     , "Summarecon Mall Bekasi (Bekasi)"
+     , "Bekasi Trade Center Mall (Bekasi)"
+     , "Summarecon Mall Karawang (Karawang)"
+     , "Resinda Park Mall Karawang (Karawang)"
+     , "Butik Synthesis Square (Jakarta Selatan)"
+     ]
     )
-    kompleks = st.radio(
-        "Anda mencari resep yang sederhana dan cepat atau yang lebih kompleks?",
-        ("Sederhana dan cepat", "Kompleks"),
-    )
-    metode = st.multiselect("Preferensi metode memasak", ["goreng", "rebus", "bakar"])
-    jenis = st.radio(
-        "Anda mencari resep untuk?", ("sarapan", "makan siang", "makan malam")
-    )
-    porsi = st.number_input(
-        "Untuk berapa orang Anda akan memasak?", min_value=1, max_value=5, step=1
-    )
+
+    # ramah_anak = st.radio("Opsi makanan ramah anak", ("Tidak", "Ya"))
+    # agama_budaya = st.text_input(
+    #     "(optional) Apakah Anda memiliki batasan seperti halal atau vegetarian?"
+    # )
+    # kompleks = st.radio(
+    #     "Anda mencari resep yang sederhana dan cepat atau yang lebih kompleks?",
+    #     ("Sederhana dan cepat", "Kompleks"),
+    # )
+    # metode = st.multiselect("Preferensi metode memasak", ["goreng", "rebus", "bakar"])
+    # jenis = st.radio(
+    #     "Anda mencari resep untuk?", ("sarapan", "makan siang", "makan malam")
+    # )
+    # porsi = st.number_input(
+    #     "Untuk berapa orang Anda akan memasak?", min_value=1, max_value=5, step=1
+    # )
 
     if st.button("Kirim"):
 
         prompt = f"""
-                Buatkan saya 3 resep masakan berbeda berdasarkan kriteria berikut ini, perlu dicatat anda tidak perlu memakai semua bahan untuk satu resep, anda bisa menambahkan bumbu-bumbu dasar pada resep, dan anda bisa menggunakan alat dapur dasar dalam proses masak.
+          Petakan saya rute untuk setiap kurir saya agar semua kurir dapat melakukan pekerjaan deliverynya cepat selesai dan seefisiensi mungkin untuk jarak dan waktu.
+          Pembagian daerah disesuaikan dengan perjalanan masing-masing kurir dari satu tempat ke tempat tujuan selanjutnya.
+          Setiap kurir akan kembali lagi ke lokasi keberangkatan sebagai tujuan akhir.
+          Berikan juga total perkiraan setiap perjalanan.
 
-                Bahan-bahan: {bahan}
-                Makanan yang sudah saya siapkan: {makanan}
-                Jenis masakan daerah: {daerah}
-                Opsi ramah anak: {ramah_anak}
-                Pertimbangan agama/budaya: {agama_budaya}
-                Kesederhanaan resep: {kompleks}
-                metode memasak: {metode}
-                jenis makanan: {jenis}
-                porsi: {porsi} orang
+          Lokasi keberangkatan: {lokasi_keberangkatan}
+          Jumlah driver: {jumlah_driver}
+          List lokasi: {list_lokasi}
 
-                Format output:
-                #[Nama Resep]
-                ##Bahan-Bahan:
-                [konten]
+          Format output:
+          #[Kurir A]
+          ##Rute-rute:
+          1. [Lokasi keberangkatan] -> [Tempat Tujuan 1] [x jam]
+          2. [Tempat Tujuan 1] -> [Tempat Tujuan 2] [x jam]
+          3. ...
+          ...
+          x. [Tempat Tujuan terakhir] -> [Lokasi keberangkatan] [x jam]
 
-                ##Cara Memasak:
-                [konten]
+          Total waktu perjalanan: x jam
 
-                ##Cara penyajian:
-                [konten]
+          #[Kurir B]
+          ##Rute-rute:
+          1. [Lokasi keberangkatan] -> [Tempat Tujuan 1] [x jam]
+          2. [Tempat Tujuan 1] -> [Tempat Tujuan 2] [x jam]
+          3. ...
+          ...
+          x. [Tempat Tujuan X] -> [Lokasi keberangkatan] [x jam]
+
+          Total waktu perjalanan: x jam
+
+          dan seterusnya.
+
+
         """
 
         # user_input += additional_prompt
